4e5d392e5527ff24022b75ff629de41ee9d8bde8
sierrapablo <pablo@metrio.es>
2025-01-08 12:25:21 +0100

    remove delay settings from App

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
 
   const handleSolveSudoku = async () => {
     const boardCopy = board.map(row => [...row]) // Crea una copia para trabajar con ella
-    await solveSudoku(boardCopy, setBoard, 5) // Resuelve con una visualización de 5ms por iteración
+    await solveSudoku(boardCopy, setBoard) // Resuelve
   }
 
   return (
